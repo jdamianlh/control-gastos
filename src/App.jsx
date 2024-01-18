34e5901496c29eff71a2b746b246cf4a61f647dc
Damian <jdamianlh@gmail.com>
2024-01-17 23:52:37 -0500

    Agregando el modal con formulario, state de gastos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import Modal from './components/Modal'
+import IconoNuevoGasto from './img/nuevo-gasto.svg'
 
 function App() {
 
   const [presupuesto, setPresupuesto] = useState(0)
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
 
+  const [modal, setModal] = useState(false)
+  const [animarModal, setAnimarModal] = useState(false)
+
+  const [gastos, setGastos] = useState([])
+
+  const handleNuevoGasto = () => {
+    setModal(true)
+
+    setTimeout(()=>{
+      setAnimarModal(true)
+    },500)
+  }
+
+  const guardarGasto = gasto => {
+    console.log(gasto);
+  }
   return (
     <div>
       <Header
@@ -14,6 +32,24 @@ function App() {
         isValidPresupuesto={isValidPresupuesto}
         setIsValidPresupuesto={setIsValidPresupuesto}
       />
+      {isValidPresupuesto && 
+        <div className='nuevo-gasto'>
+          <img 
+            src={IconoNuevoGasto} 
+            alt="icono nuevo gasto" 
+            onClick={handleNuevoGasto}  
+          />
+        </div>
+      }
+
+      {modal && 
+      <Modal
+        setModal={setModal}
+        animarModal={animarModal}
+        setAnimarModal={setAnimarModal}
+        guardarGasto={guardarGasto}
+      /> }
+      
     </div>
   )
 }
